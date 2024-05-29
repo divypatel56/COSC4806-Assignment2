0b99506a990db75c04635dc2b3541134965fb4ec
Divy Patel <67093305+divypatel56@users.noreply.github.com>
2024-05-29 14:17:23 +0000

    Created config file

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+//Defining the variables
+define('DB_HOST',     '38r.h.filess.io');
+define('DB_USER',     'COSC4806_changingus');
+define('DB_PASS',     $_ENV['DB_PASS']);
+define('DB_DATABASE', 'COSC4806_changingus');
+define('DB_PORT',     '3305');
+
+?>
